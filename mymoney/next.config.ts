cb17a5f5a790ebc4f23f39159b45e8a1f08db075
MiguellArcanjo <contato.miguelarcanjo2305@gmail.com>
2025-06-25 20:44:21 -0300

    ..

--- mymoney/next.config.ts
+++ mymoney/next.config.ts
@@ -1,7 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config: any) => {
-    config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
